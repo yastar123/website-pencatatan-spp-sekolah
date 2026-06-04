64077f33649213d148b5eb3f238b53bfbe582495
yastar123 <eduj2164@gmail.com>
2026-06-04 20:26:32 +0700

    fix web pencatatan spp sekolah

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="" suppressHydrationWarning>
       <body>
         <ThemeProvider>
           <AppShell>{children}</AppShell>
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -7,5 +7,5 @@ import {
 } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
